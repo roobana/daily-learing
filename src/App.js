45f12f88c3629631c154d35d66eb9caef7d9f6d3
roobana <roobanachinraj@gmail.com>
2024-06-11 15:13:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
 
-function App() {
+export default function App() {
+  const [show, setShow] = useState(true);
+  const handleButton = () => {
+    // if (hide) {
+    //   setHide(false);
+    // } else {
+    //   setHide(true);
+    // }
+    show ? setShow(false) : setShow(true);
+    // (show && setShow(false)) || setShow(true);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        {/* {hide ? <p>Hello World</p> : null} */}
+        <p className={show ? "show" : "hide"}>Hello World</p>
+        <button className="clickButton" onClick={handleButton}>
+          Click Me
+        </button>
+        {/* <button className="hideButton" onClick={() => setHide(false)}>
+          Hide
+        </button>
+        <button className="showButton" onClick={() => setHide(true)}>
+          Show
+        </button> */}
+      </div>
+    </>
   );
 }
-
-export default App;
